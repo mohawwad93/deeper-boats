47610f28038fd047ad6235e46a062b83416541d6
Mohammed Awwad <mohammedawwad@Mohammeds-MacBook-Pro.local>
2021-12-10 00:10:15 +0200

    Updates

---
+++ precache-manifest.0d249ec2b5a12571ccc7f5b891fc9f4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "985010ab5e0b0ee67e7c",
+    "url": "/deeper-boats/static/css/main.3365d089.chunk.css"
+  },
+  {
+    "revision": "985010ab5e0b0ee67e7c",
+    "url": "/deeper-boats/static/js/main.985010ab.chunk.js"
+  },
+  {
+    "revision": "ddeff198da948d7160b8",
+    "url": "/deeper-boats/static/js/1.ddeff198.chunk.js"
+  },
+  {
+    "revision": "fe1c88a194ae5af94d4d",
+    "url": "/deeper-boats/static/js/runtime~main.fe1c88a1.js"
+  },
+  {
+    "revision": "c4a777603529d2bddfcbc41dd3c545df",
+    "url": "/deeper-boats/index.html"
+  }
+];
